e2d177691e113270d01a3c961029cc241191ccf6
Cassie Zhang <xiyanzhang9@gmail.com>
2024-04-21 21:19:08 -0400

    used map

--- message_serialization.h
+++ message_serialization.h
@@ -2,10 +2,17 @@
 #define MESSAGE_SERIALIZATION_H
 
 #include "message.h"
+#include <map>
 
 namespace MessageSerialization {
-  void encode(const Message &msg, std::string &encoded_msg);
-  void decode(const std::string &encoded_msg, Message &msg);
+    extern const std::map<MessageType, std::string> typeToStringMap;
+    extern const std::map<std::string, MessageType> stringToTypeMap;
+
+    std::string messageTypeToString(MessageType type);
+    MessageType stringToMessageType(const std::string& typeStr);
+    
+    void encode(const Message &msg, std::string &encoded_msg);
+    void decode(const std::string &encoded_msg, Message &msg);
 };
 
 #endif // MESSAGE_SERIALIZATION_H
