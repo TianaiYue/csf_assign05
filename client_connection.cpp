94922af864a861a5e228c848e4cec622e02b0d0b
Tianai Yue'git config --global user.name Tianai <tyue4@jh.edu>
2024-04-26 02:59:30 -0400

    clientconnection

--- client_connection.cpp
+++ client_connection.cpp
@@ -1,172 +1,142 @@
 /*
  * A5MS1
- * The ser
+ * The client connection
  * Cassie Zhang xzhan304
  * Tianai Yue tyue4
  */
 
 #include <iostream>
 #include <cassert>
-#include <memory>
 #include "csapp.h"
-#include "exceptions.h"
-#include "guard.h"
+#include "message.h"
+#include "message_serialization.h"
 #include "server.h"
+#include "exceptions.h"
+#include "client_connection.h"
 
-// Server::Server()
-//   // TODO: initialize member variables
-// {
-//   // TODO: implement
-// }
-
-Server::Server() {
-    pthread_mutex_init(&mutex, nullptr);
+ClientConnection::ClientConnection( Server *server, int client_fd )
+  : m_server( server )
+  , m_client_fd( client_fd )
+{
+    rio_readinitb( &m_fdbuf, m_client_fd );
 }
 
-Server::~Server()
+ClientConnection::~ClientConnection()
 {
     // TODO: implement
-    running = false;
-    Close(listenfd);
-    pthread_mutex_destroy(&mutex);
+    close_connection(); 
 }
 
-void Server::listen( const std::string &port )
+void ClientConnection::chat_with_client()
 {
-  // TODO: implement
-  listenfd = open_listenfd(port.c_str());
-    if (listenfd < 0) {
-        log_error("Failed to listen on port: " + port);
-        exit(1);
-    }
-}
-
-void Server::server_loop()
-{
-  // TODO: implement
-
-  // Note that your code to start a worker thread for a newly-connected
-  // client might look something like this:
-/*
-  ClientConnection *client = new ClientConnection( this, client_fd );
-  pthread_t thr_id;
-  if ( pthread_create( &thr_id, nullptr, client_worker, client ) != 0 )
-    log_error( "Could not create client thread" );
-*/
-    std::cout << "Server is running and waiting for connections..." << std::endl;
-    sockaddr_storage clientaddr;
-    socklen_t clientlen = sizeof(sockaddr_storage);
-    pthread_t tid;
-
-    while (running) {
-        int clientfd = accept(listenfd, (sockaddr *)&clientaddr, &clientlen);
-        if (clientfd < 0) {
-            log_error("Failed to accept client connection");
-            continue;
+    // TODO: implement
+    try {
+        while (true) {
+            Message request = read_message();
+            if (request.get_message_type() == MessageType::BYE) {
+                send_message(Message(MessageType::OK));
+                break;
+            }
+            handle_request(request);
         }
+    } catch (const std::exception &e) {
+        std::cerr << "Exception in client connection: " << e.what() << std::endl;
+        send_message(Message(MessageType::ERROR, {"Internal server error"})); 
+    }
+    close_connection();
+}
 
-        auto clientConn = new ClientConnection(this, clientfd);
-        pthread_t client_thread;
-        if (pthread_create(&client_thread, nullptr, client_worker, clientConn) != 0) {
-            log_error("Could not create client thread");
-            delete clientConn;
-        } else {
-            pthread_detach(client_thread);
-        }
+// TODO: additional member functions
+Message ClientConnection::read_message() {
+    char buf[Message::MAX_ENCODED_LEN];
+    if (rio_readlineb(&m_fdbuf, buf, sizeof(buf)) <= 0) {
+        throw CommException("Failed to read from client");
+    }
+    Message msg;
+    MessageSerialization::decode(std::string(buf), msg);
+    if (!msg.is_valid()) {
+        throw InvalidMessage("Received invalid message");
+    }
+    return msg;
+}
+
+void ClientConnection::send_message(const Message& msg) {
+    std::string encoded_msg;
+    MessageSerialization::encode(msg, encoded_msg);
+    if (rio_writen(m_client_fd, encoded_msg.c_str(), encoded_msg.length()) < 0) {
+        throw CommException("Failed to send message to client");
+    }
+}
+
+void ClientConnection::handle_request(const Message& request) {
+    if (!request.is_valid()) {
+        send_message(Message(MessageType::ERROR, {"Invalid message format"}));
+        return;
+    }
+    try {
+        switch (request.get_message_type()) {
+            case MessageType::LOGIN: {
+                std::string username = request.get_username();
+                if (!username.empty()) {
+                    send_message(Message(MessageType::OK));
+                } else {
+                    send_message(Message(MessageType::ERROR, {"Invalid login format"}));
+                }
+                break;
+            }
+            case MessageType::BEGIN: {
+                m_server->begin_transaction(m_client_fd);
+                send_message(Message(MessageType::OK));
+                break;
+            }
+            case MessageType::COMMIT: {
+                m_server->commit_transaction(m_client_fd);
+                send_message(Message(MessageType::OK));
+                break;
+            }
+            case MessageType::SET: {
+                Table* table = m_server->find_table(request.get_table());
+                if (!table) {
+                    send_message(Message(MessageType::ERROR, {"Unknown table"}));
+                    return;
+                }
+                table->lock();
+                table->set(request.get_key(), request.get_value());
+                table->commit_changes();
+                table->unlock();
+                send_message(Message(MessageType::OK));
+                break;
+            }
+            case MessageType::GET: {
+                Table* table = m_server->find_table(request.get_table());
+                if (!table) {
+                    send_message(Message(MessageType::ERROR, {"Unknown table"}));
+                    return;
+                }
+
+                table->lock();
+                try {
+                    std::string value = table->get(request.get_key());
+                    table->unlock();
+                    send_message(Message(MessageType::DATA, {value}));
+                } catch (const OperationException& e) {
+                    table->unlock();
+                    send_message(Message(MessageType::ERROR, {e.what()}));
+                }
+                break;
+            }
+            default:
+                send_message(Message(MessageType::ERROR, {"Unsupported operation"}));
+                break;
+            }
+    } catch (const std::exception& e) {
+        send_message(Message(MessageType::ERROR, {"Internal server error"}));
+        m_server->rollback_transaction(m_client_fd);
     }
 }
 
 
-void *Server::client_worker( void *arg )
-{
-  // TODO: implement
-
-  // Assuming that your ClientConnection class has a member function
-  // called chat_with_client(), your implementation might look something
-  // like this:
-/*
-  std::unique_ptr<ClientConnection> client( static_cast<ClientConnection *>( arg ) );
-  client->chat_with_client();
-  return nullptr;
-*/
-    auto client = static_cast<ClientConnection *>(arg);
-    client->chat_with_client();
-    delete client;
-    return nullptr;
-}
-
-void Server::log_error( const std::string &what )
-{
-  std::cerr << "Error: " << what << "\n";
-}
-
-// TODO: implement member functions
-bool Server::create_table(const std::string &name) {
-    Guard guard(mutex);
-    if (tables.find(name) == tables.end()) {
-        tables.emplace(std::piecewise_construct,
-                       std::forward_as_tuple(name),
-                       std::forward_as_tuple(name));
-        return true;
-    }
-    return false;
-}
-
-Table* Server::find_table(const std::string &name) {
-    Guard guard(mutex);
-    auto it = tables.find(name);
-    if (it != tables.end()) {
-        return &it->second;
-    }
-    return nullptr;
-}
-
-void Server::begin_transaction(int client_id) {
-    pthread_mutex_lock(&mutex);
-    in_transaction[client_id] = true;
-    pthread_mutex_unlock(&mutex);
-}
-
-void Server::commit_transaction(int client_id) {
-    pthread_mutex_lock(&mutex);
-    if (in_transaction[client_id]) {
-        for (const auto& table_name : transaction_locks[client_id]) {
-            tables[table_name].commit_changes();
-            unlock_table(table_name, client_id);
-        }
-        in_transaction[client_id] = false;
-    }
-    pthread_mutex_unlock(&mutex);
-}
-
-void Server::rollback_transaction(int client_id) {
-    pthread_mutex_lock(&mutex);
-    if (in_transaction[client_id]) {
-        for (const auto& table_name : transaction_locks[client_id]) {
-            tables[table_name].rollback_changes();
-            unlock_table(table_name, client_id);
-        }
-        in_transaction[client_id] = false;
-    }
-    pthread_mutex_unlock(&mutex);
-}
-
-bool Server::lock_table(const std::string& table_name, int client_id) {
-    auto it = tables.find(table_name);
-    if (it == tables.end()) {
-        return false; 
-    }
-    if (it->second.trylock()) {
-        transaction_locks[client_id].insert(table_name);
-        return true;
-    }
-    return false;
-}
-
-void Server::unlock_table(const std::string& table_name, int client_id) {
-    auto it = tables.find(table_name);
-    if (it != tables.end() && transaction_locks[client_id].find(table_name) != transaction_locks[client_id].end()) {
-        it->second.unlock();
-        transaction_locks[client_id].erase(table_name); 
-    }
+void ClientConnection::close_connection() {
+    Close(m_client_fd);
+    std::cout << "Connection with client " << m_client_fd << " closed." << std::endl;
 }
